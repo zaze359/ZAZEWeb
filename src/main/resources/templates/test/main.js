903b1a03cf16ccf8500298fb24cddd3226b2cc0a
zaze <359635919@qq.com>
2020-07-14 18:08:05 +0800

    1

---
+++ src/main/resources/templates/test/main.js
@@ -0,0 +1,8 @@
+// import Vue from 'vue';
+// import Hello from "./App.vue";
+ 
+// new Vue({
+//     el: "#box",
+//     template: '<Hello/>',
+//     components: { Hello }
+// })
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
-// webpack.config.js
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+// // webpack.config.js
+// const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
-module.exports = {
-  module: {
-    rules: [
-      // ... 其它规则
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      }
-    ]
-  },
-  plugins: [
-    // 请确保引入这个插件！
-    new VueLoaderPlugin()
-  ]
-}
+// module.exports = {
+//     entry: "./main.js",     // 入口js文件
+//     output: {
+//         path: __dirname,
+//         filename: "build.js"    // 输出js文件
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.vue$/,
+//                 loader: 'vue-loader'
+//             },
+//             // 它会应用到普通的 `.js` 文件
+//             // 以及 `.vue` 文件中的 `<script>` 块
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader'
+//             },
+//             // 它会应用到普通的 `.css` 文件
+//             // 以及 `.vue` 文件中的 `<style>` 块
+//             {
+//                 test: /\.css$/,
+//                 use: [
+//                     'vue-style-loader',
+//                     'css-loader'
+//                 ]
+//             }
+//         ]
+//     },
+//     resolve: {
+//         alias: {
+//             'vue$': 'vue/dist/vue.common.js'
+//         }
+//     },
+//     plugins: [
+//         // 请确保引入这个插件！
+//         new VueLoaderPlugin()
+//     ]
+// }
