33ac70722c998ca926934eb9ab0366303a2b383c
zaze <359635919@qq.com>
2021-03-18 18:07:04 +0800

    1

--- src/main/java/com/zaze/server/comm/aop/LoggerManage.java
+++ src/main/java/com/zaze/server/comm/aop/LoggerManage.java
@@ -10,6 +10,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface LoggerManage {
-
-	public String description();
+    String description();
 }
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,4 +1,11 @@
 function createFrame(url) {
-    return '<iframe scrolling="auto" frameborder="0"  src="' + url
-        + '" style="width:100%;height:100%;"></iframe>';
+    let height = document.documentElement.clientHeight - 160 + "px";
+    return "<iframe onload='$(this)[0].height = document.documentElement.clientHeight-160' width='100%' height='" + height + "' frameborder='0' scrolling='auto' src=\"" + url + "\"></iframe>"
+    // return '<iframe scrolling="auto" frameborder="0"  src="' + url
+    //     + '" style="width:100%;height:100%;"></iframe>';
+}
+
+
+function addTab(url) {
+
 }
